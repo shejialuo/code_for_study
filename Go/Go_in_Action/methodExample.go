5447b37374cb6eaf38e53a9900beab6942fa004b
shejialuo <shejialuo@gmail.com>
2021-10-28 22:38:26 +0800

    Finish go in action

---
+++ Go/Go_in_Action/methodExample.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type user struct {
+  name string
+  email string
+}
+
+// notify implements a method with a value receiver.
+func (u user) notify() {
+  fmt.Printf("Sending User Email To %s<%s>\n", u.name, u.email)
+}
+
+// changeEmail implements a method with a pointer receiver
+func (u *user) changeEmail(email string) {
+  u.email = email
+}
+
+func main() {
+  // Values of type user can be used to call methods
+  // declared with a value receiver
+  bill := user{"Bill", "bill@email.com"}
+  bill.notify()
+
+  // Pointers of type user can be used to call methods
+  // declared with a value receiver.
+  lisa := &user{"Lisa", "lisa@email.com"}
+  lisa.notify()
+
+  // Values of type user can be used to call methods
+  // declared with a pointer receiver
+  bill.changeEmail("bill@newdomain.com")
+  bill.notify()
+
+  // Pointers of type user can be used to call methods
+  // declared with a pointer receiver.
+  lisa.changeEmail("lisa@comcast.com")
+  lisa.notify()
+}
