147a723618afb6b6ee71062073b4aaa8ceb81276
shejialuo <shejialuo@gmail.com>
2021-11-13 11:15:04 +0800

    Finish chapter 3

---
+++ Scala/Programming_in_Scala/chapter 3/listExample.scala
@@ -0,0 +1,4 @@
+val oneTwo = List(1, 2)
+val threeFour = List(3,4)
+
+println(oneTwo + " and " + threeFour + " were not mutated.")
---
+++ Scala/Programming_in_Scala/chapter 3/parameterizedObject.scala
@@ -0,0 +1,4 @@
+val greetStrings = Array("zero", "one", "two")
+
+for (i <- 0 to 2)
+  println(greetStrings(i))
